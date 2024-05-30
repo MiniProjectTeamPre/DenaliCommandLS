36aa6630a75b44c209ca0d085b707a87a5ce9f55
AnochaPay <Anocha_ping@hotmail.com>
2024-05-30 16:28:27 +0700

    add

---
+++ DenaliCommandLS/FormMain.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace DenaliCommandLS {
+    partial class FormMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pn_upLoad1 = new System.Windows.Forms.Panel();
+            this.cb_debug = new System.Windows.Forms.CheckBox();
+            this.bt_config = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.lb_processWait = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1466, 92);
+            this.panel1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.White;
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tabControl1);
+            this.groupBox1.Controls.Add(this.lb_processWait);
+            this.groupBox1.Controls.Add(this.panel5);
+            this.groupBox1.Controls.Add(this.panel4);
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.cb_debug);
+            this.groupBox1.Controls.Add(this.bt_config);
+            this.groupBox1.Controls.Add(this.pn_upLoad1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1466, 802);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(1386, 923);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "V2023.01";
+            // 
+            // pn_upLoad1
+            // 
+            this.pn_upLoad1.BackColor = System.Drawing.Color.White;
+            this.pn_upLoad1.Location = new System.Drawing.Point(861, 169);
+            this.pn_upLoad1.Name = "pn_upLoad1";
+            this.pn_upLoad1.Size = new System.Drawing.Size(560, 102);
+            this.pn_upLoad1.TabIndex = 1;
+            // 
+            // cb_debug
+            // 
+            this.cb_debug.AutoSize = true;
+            this.cb_debug.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cb_debug.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cb_debug.Location = new System.Drawing.Point(1208, 85);
+            this.cb_debug.Name = "cb_debug";
+            this.cb_debug.Size = new System.Drawing.Size(96, 34);
+            this.cb_debug.TabIndex = 10;
+            this.cb_debug.Text = "Debug";
+            this.cb_debug.UseVisualStyleBackColor = true;
+            // 
+            // bt_config
+            // 
+            this.bt_config.BackgroundImage = global::DenaliCommandLS.Properties.Resources.Config;
+            this.bt_config.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bt_config.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bt_config.Location = new System.Drawing.Point(1321, 36);
+            this.bt_config.Name = "bt_config";
+            this.bt_config.Size = new System.Drawing.Size(100, 83);
+            this.bt_config.TabIndex = 9;
+            this.bt_config.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(861, 277);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(560, 102);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(861, 385);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(560, 102);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(861, 493);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(560, 102);
+            this.panel4.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(861, 601);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(560, 102);
+            this.panel5.TabIndex = 11;
+            // 
+            // lb_processWait
+            // 
+            this.lb_processWait.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lb_processWait.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_processWait.Location = new System.Drawing.Point(1237, 717);
+            this.lb_processWait.Name = "lb_processWait";
+            this.lb_processWait.Size = new System.Drawing.Size(184, 35);
+            this.lb_processWait.TabIndex = 13;
+            this.lb_processWait.Text = "Process wait (0)";
+            this.lb_processWait.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(55, 195);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(660, 259);
+            this.tabControl1.TabIndex = 14;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(652, 233);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "  Page1  ";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(652, 233);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "  Page2  ";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(54, 63);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(661, 114);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "DryIce Programming\r\nSelect Hex File and Number of ST-Link\r\nClick Upload and wait " +
+    "to success";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(652, 233);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "  Page3  ";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(652, 233);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "  Page4  ";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(55, 510);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(660, 253);
+            this.richTextBox1.TabIndex = 16;
+            this.richTextBox1.Text = "";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1490, 961);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Denali LS Command";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Resize += new System.EventHandler(this.FormMain_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lb_processWait;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        public System.Windows.Forms.CheckBox cb_debug;
+        private System.Windows.Forms.Button bt_config;
+        private System.Windows.Forms.Panel pn_upLoad1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+    }
+}
+
---
+++ DenaliCommandLS/FormMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DenaliCommandLS {
+    public partial class FormMain : Form {
+        public FormMain() {
+            InitializeComponent();
+            resizeAuto = new ResizeAuto(this);
+        }
+
+        #region =============================================== Input ===============================================
+        private ResizeAuto resizeAuto;
+        #endregion
+
+        #region =============================================== Display ===============================================
+
+        #endregion
+
+        #region =============================================== Cal ===============================================
+
+        #endregion
+
+        #region =============================================== Main ===============================================
+        private void FormMain_Load(object sender, EventArgs e) {
+
+
+
+            resizeAuto.Initial();
+        }
+        private void FormMain_Resize(object sender, EventArgs e) {
+            resizeAuto.Resize();
+            resizeAuto.Resize();
+        }
+        #endregion
+
+
+    }
+}
